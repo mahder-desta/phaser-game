ecd893b059e2107d8e67d6b0da88d04ea40e280a
Mahder Desta <mahderds2013@gmail.com>
2023-12-11 11:33:50 +0300

    feat: drag and drop implemented

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,291 @@
 import * as Phaser from "phaser";
+
+class Rack extends Phaser.GameObjects.GameObject {
+  sprite: Phaser.GameObjects.Sprite;
+  initialPosition: Phaser.Math.Vector2;
+  capacity: number;
+  currentMiners: number;
+  miners: Miner[];
+  constructor(scene: MainScene, x: number, y: number, image: string, capacity) {
+    super(scene, "Rack");
+    this.capacity = capacity;
+    this.currentMiners = 0;
+    this.miners = [];
+    this.sprite = scene.add
+      .sprite(x, y, image)
+      .setInteractive({ draggable: true });
+    this.sprite.displayHeight = 50;
+    this.sprite.displayWidth = 50;
+    scene.panel.add(this.sprite);
+
+    // Set up events for drag start and end
+    this.sprite.on("dragstart", () => {
+      this.initialPosition = new Phaser.Math.Vector2(
+        this.sprite.x,
+        this.sprite.y
+      );
+
+      this.sprite.setScale(1.2);
+    });
+
+    this.sprite.on("dragend", () => {
+      if (!this.isValidToDrop(scene)) {
+        this.sprite.setScale(0.5);
+        // Return to initial position if not dropped on the drop zone
+        this.sprite.x = this.initialPosition.x;
+        this.sprite.y = this.initialPosition.y;
+      } else {
+        this.sprite.setScale(1.3);
+        scene.racks.push(this);
+      }
+    });
+
+    scene.input.setDraggable(this.sprite);
+  }
+  private isValidToDrop(scene: MainScene): boolean {
+    if (scene.racks.length >= 8) return false;
+
+    // check overlap
+    const newRackBounds = this.sprite.getBounds();
+    const overlap = scene.racks.some((rack: Rack) => {
+      if (rack !== this) {
+        const existingRackBounds = rack.sprite.getBounds();
+        return Phaser.Geom.Intersects.RectangleToRectangle(
+          newRackBounds,
+          existingRackBounds
+        );
+      }
+      return false;
+    });
+
+    if (overlap) {
+      return false;
+    }
+    // Check if the rack overlap with the room drop zone
+    const rackBounds = this.sprite.getBounds();
+    const dropZoneBounds = scene.roomDropZone.getBounds();
+    return Phaser.Geom.Intersects.RectangleToRectangle(
+      rackBounds,
+      dropZoneBounds
+    );
+  }
+
+  dropMiner(miner: Miner) {
+    if (this.miners.length < this.capacity) {
+      this.miners.push(miner);
+    }
+  }
+}
+
+class Miner extends Phaser.GameObjects.GameObject {
+  sprite: Phaser.GameObjects.Sprite;
+
+  constructor(scene: MainScene, x: number, y: number, image: string) {
+    super(scene, "Miner");
+    this.sprite = scene.add
+      .sprite(x, y, image)
+      .setInteractive({ draggable: true });
+    this.sprite.displayHeight = 40;
+    this.sprite.displayWidth = 80;
+
+    scene.panel.add(this.sprite);
+
+    this.sprite.on("dragend", () => {
+      this.checkDrop(scene);
+    });
+
+    scene.input.setDraggable(this.sprite);
+  }
+
+  private checkDrop(scene: MainScene) {
+    // Check if the miner is dropped on any rack in the drop zone
+    const droppedOnRack: Rack = scene.racks.find((rack: Rack) => {
+      const minerBounds = this.sprite.getBounds();
+      const rackBounds = rack.sprite.getBounds();
+      const overlappingMiner = rack.miners.find(
+        (miner) =>
+          miner !== this &&
+          Phaser.Geom.Intersects.RectangleToRectangle(
+            miner.sprite.getBounds(),
+            minerBounds
+          )
+      );
+      return (
+        Phaser.Geom.Intersects.RectangleToRectangle(minerBounds, rackBounds) &&
+        !overlappingMiner &&
+        rack.miners.length < rack.capacity
+      );
+    });
+
+    if (droppedOnRack) {
+      droppedOnRack.dropMiner(this);
+    } else {
+      // Return to initial position if not dropped on a rack
+      this.sprite.x = this.sprite.input.dragStartX;
+      this.sprite.y = this.sprite.input.dragStartY;
+    }
+  }
+}
+
+type RackType = {
+  name: string;
+  capacity: number;
+};
 class MainScene extends Phaser.Scene {
-  private draggableObjects: Phaser.GameObjects.Sprite[] = [];
-  private dropZone!: Phaser.GameObjects.Zone;
+  public roomDropZone!: Phaser.GameObjects.Zone;
+
+  panel: Phaser.GameObjects.Container;
+  racksInStore: RackType[] = [];
+  minersInStore: string[] = [];
+  racks: Rack[] = [];
+  miners: Phaser.GameObjects.Image[];
+
+  private gameWidth: number;
+  private gameHeight: number;
 
   constructor() {
     super({ key: "MainScene" });
+    this.racks = [];
+    this.miners = [];
+    this.racksInStore = [
+      { name: "rack_3", capacity: 3 },
+      { name: "rack_3", capacity: 3 },
+      { name: "rack_3", capacity: 3 },
+      { name: "rack_3", capacity: 3 },
+      { name: "rack_4", capacity: 4 },
+      { name: "rack_4", capacity: 4 },
+      { name: "rack_4", capacity: 4 },
+      { name: "rack_4", capacity: 4 },
+      { name: "rack_4", capacity: 4 },
+    ];
+
+    this.minersInStore = [
+      "aurum",
+      "cp_106",
+      "crypto_neko",
+      "dj_roller",
+      "game_boy",
+      "shifter",
+      "think_tronik",
+    ];
   }
 
   preload() {
-    this.load.image("banana", "assets/banana.png");
+    this.gameWidth = this.sys.game.config.width as number;
+    this.gameHeight = this.sys.game.config.height as number;
+
+    this.load.image("avatar", "assets/avatar.png");
     this.load.image("background", "assets/bg.png");
+    //racks
+    this.load.image("rack_3", "assets/rack_3.png");
+    this.load.image("rack_4", "assets/rack_4.png");
+
+    //miners
+    this.load.image("aurum", "assets/aurum.gif");
+    this.load.image("cp_106", "assets/cp_106.gif");
+    this.load.image("crypto_neko", "assets/crypto_neko.gif");
+    this.load.image("dj_roller", "assets/dj_roller.gif");
+    this.load.image("etherilliant", "assets/etherilliant.gif");
+    this.load.image("first_class", "assets/first_class.gif");
+    this.load.image("game_boy", "assets/game_boy.gif");
+    this.load.image("shifter", "assets/shifter.gif");
+    this.load.image("steamwheedle", "assets/steamwheedle.gif");
+    this.load.image("think_tronik", "assets/think_tronik.gif");
   }
 
   create() {
-    this.add.image(0, 0, "background").setOrigin(0, 0);
-    const blockWidth = 100; // Set the width of the blocks
-    const blockHeight = 100; // Set the height of the blocks
-    // Create draggable items
-    for (let i = 0; i < 3; i++) {
-      const draggableObject = this.add
-        .sprite(100 + i * 150, 100, "banana")
-        .setInteractive({ draggable: true });
+    var background = this.add.image(0, 0, "background").setOrigin(0, 0);
+    var gameWidth = Number(this.sys.game.config.width);
+    var gameHeight = Number(this.sys.game.config.height);
+    background.setScale(
+      gameWidth / background.width,
+      gameHeight / background.height
+    );
 
-      draggableObject.displayWidth = blockWidth;
-      draggableObject.displayHeight = blockHeight;
+    // Add Avatar
+    var avatar = this.add.image(200, 424, "avatar");
+    avatar.setOrigin(0.5, 0.5);
+    avatar.displayHeight = 55;
+    avatar.displayWidth = 55;
 
-      this.draggableObjects.push(draggableObject);
+    //add bottom panel
+    this.panel = this.add.container(0, gameHeight - 60);
+    const panelBackground = this.add.graphics();
+    panelBackground.fillStyle(0x222222, 0.7);
+    panelBackground.fillRect(0, 0, gameWidth, 60);
+    this.panel.add(panelBackground);
 
-      this.input.setDraggable(draggableObject);
+    //racks
+    this.racksInStore.forEach((rack, i) => {
+      new Rack(this, 40 + i * 40, 30, rack.name, rack.capacity);
+    });
 
-      this.input.on(
-        "dragstart",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: Phaser.GameObjects.GameObject
-        ) => {}
-      );
+    //miners
+    this.minersInStore.forEach((miner, i) => {
+      new Miner(this, 80 + this.racksInStore.length * 40 + i * 80, 30, miner);
+    });
 
-      this.input.on(
-        "drag",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: Phaser.GameObjects.GameObject,
-          dragX: number,
-          dragY: number
-        ) => {
-          if (gameObject instanceof Phaser.GameObjects.Sprite) {
-            gameObject.x = dragX;
-            gameObject.y = dragY;
-          }
-        }
-      );
+    this.setupDragInput();
+    this.createRoomDropZone();
+  }
 
-      this.input.on("dragend", () => {});
-    }
-
-    // drop zone
-    this.dropZone = this.add
-      .zone(400, 400, 600, 200)
-      .setRectangleDropZone(600, 200);
-
+  setupDragInput() {
     this.input.on(
-      "drop",
+      "dragstart",
       (
         pointer: Phaser.Input.Pointer,
         gameObject: Phaser.GameObjects.GameObject
-      ) => {
-        if (
-          gameObject instanceof Phaser.GameObjects.Sprite &&
-          this.dropZone.getBounds().contains(gameObject.x, gameObject.y)
-        ) {
-          const gridSpacing = 100;
-          const snappedX = Math.round(gameObject.x / gridSpacing) * gridSpacing;
-          const snappedY = Math.round(gameObject.y / gridSpacing) * gridSpacing;
+      ) => {}
+    );
 
-          gameObject.x = snappedX;
-          gameObject.y = snappedY;
+    this.input.on(
+      "drag",
+      (
+        pointer: Phaser.Input.Pointer,
+        gameObject: Phaser.GameObjects.GameObject,
+        dragX: number,
+        dragY: number
+      ) => {
+        if (gameObject instanceof Phaser.GameObjects.Sprite) {
+          gameObject.x = dragX;
+          gameObject.y = dragY;
         }
       }
     );
+
+    this.input.on("dragend", () => {});
+  }
+
+  createRoomDropZone() {
+    const dropZoneX = this.gameWidth / 3;
+
+    this.roomDropZone = this.add
+      .zone(dropZoneX, 0, this.gameWidth / 2, this.gameHeight - 60)
+      .setOrigin(0, 0);
+    this.roomDropZone.setInteractive();
+
+    this.input.on("gameobjectover", (pointer, gameObject) => {
+      if (gameObject === this.roomDropZone) {
+      }
+    });
+
+    this.input.on("gameobjectout", (pointer, gameObject) => {
+      if (gameObject === this.roomDropZone) {
+      }
+    });
+
+    this.input.on("drop", (pointer, gameObject) => {
+      if (gameObject === this.roomDropZone) {
+      }
+    });
   }
 }
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 500,
+  width: window.innerWidth - 300,
+  height: window.innerHeight,
+  parent: "room-container",
   scene: MainScene,
   scale: {
     mode: Phaser.Scale.ScaleModes.RESIZE,
